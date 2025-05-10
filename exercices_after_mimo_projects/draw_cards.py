bed056a858b03d479111411d7ecc64941be931d5
Thomas Schwab <thomasschwab146@gmail.com>
2025-05-10 21:41:58 +0200

    Adding notes

--- exercices_after_mimo_projects/draw_cards.py
+++ exercices_after_mimo_projects/draw_cards.py
@@ -1,16 +1,6 @@
 import random
 
-
-def draw_card(deck, num_cards):
-  hand = []
-  for _ in range(num_cards):
-    if deck:
-      hand.append(deck.pop())
-    else:
-      break
-  return hand, deck
-
-
+# Creates random deck of cards
 def create_deck():
   suits = ["♥", "♦", "♣", "♠"]
   ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
@@ -22,7 +12,17 @@ def create_deck():
 
   return deck
 
+# Draws a card from the deck
+def draw_card(deck, num_cards):
+  hand = []
+  for _ in range(num_cards):
+    if deck:
+      hand.append(deck.pop())
+    else:
+      break
+  return hand, deck
 
+# Enables to show cards visually in terminal
 def show_card(card):
     space = " "
     if len(card[1]) == 2:
@@ -37,11 +37,15 @@ def show_card(card):
     +-------+
     """)
 
+# Creates the deck
 deck = create_deck()
+
+# Loop giving specific hands according to user request through input
 while len(deck) > 0:
   num_cards = int(input("How many cards do you want to draw? "))
   hand, deck = draw_card(deck, num_cards)
   for card in hand:
     show_card(card)
 
+# Declares end of deck
 print("We are out of cards")
